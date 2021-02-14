b3949314c9448a7fd1fcdb1d8cdcb28984aa3420
inappropriatename123899 <carlos_25-05-96@hotmail.com>
2021-02-14 18:41:25 -0300

    rutas creadas

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,87 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route ,Switch , Link, Redirect } from "react-router-dom";
+import Login from "./layout/login/Login"
+import ListaClientes from "./layout/clientes/ListaClientes"
+import NuevoCliente from "./layout/clientes/NuevoCliente"
+import ListaEmpleados from "./layout/empleados/ListaEmpleados"
+import NuevoEmpleado from "./layout/empleados/NuevoEmpleado"
+import Informes from "./layout/informes/Informes"
+import Perfiles from "./layout/perfiles/Perfiles"
+import ListaProyectos from "./layout/proyectos/ListaProyectos"
+import NuevoProyecto from "./layout/proyectos/NuevoProyecto"
+import ListaTareas from "./layout/tareas/ListaTareas"
+import NuevaTarea from "./layout/tareas/NuevaTarea"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>test</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+          <ul>
+            <li>
+              <Link to="/login">login</Link>
+            </li>
+            <li>
+              <Link to="/lista-tareas">Tareas</Link>
+            </li>
+            <li>
+              <Link to="/nueva-tarea">Crear Tarea</Link>
+            </li>
+            <li>
+              <Link to="/informes">Informes</Link>
+            </li>
+            <li>
+              <Link to="/lista-proyectos">Proyectos</Link>
+            </li>
+            <li>
+              <Link to="/nuevo-proyecto">Nuevo Proyecto</Link>
+            </li>
+            <li>
+              <Link to="/lista-clientes">Clientes</Link>
+            </li>
+            <li>
+              <Link to="/nuevo-cliente">Nuevo Cliente</Link>
+            </li>
+          </ul>
+        </nav>
+    <Switch>
+        <Route exact path="/login">
+          <Login/>
+        </Route>
+        <Route exact path="/lista-tareas">
+          <ListaTareas/>
+        </Route>
+        <Route exact path="/nueva-tarea">
+          <NuevaTarea/>
+        </Route>
+        <Route exact path="/informes">
+          <Informes/>
+        </Route>
+        <Route exact path="/lista-proyectos">
+          <ListaProyectos/>
+        </Route>
+        <Route exact path="/nuevo-proyecto">
+          <NuevoProyecto/>
+        </Route>
+        <Route exact path="/lista-clientes">
+          <ListaClientes/>
+        </Route>
+        <Route exact path="/nuevo-cliente">
+          <NuevoCliente/>
+        </Route>
+        <Route exact path="/lista-empleados">
+          <ListaEmpleados/>
+        </Route>
+        <Route exact path="/nuevo-empleado">
+          <NuevoEmpleado/>
+        </Route>
+        <Redirect from="*" to="/lista-proyectos"/>
+
+        {/* HABLAR CON OMAR SOBRE SI PERFILES TIENE MENÚ (MEPA Q NO) */}
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
+// http://localhost:27195
